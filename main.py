e7e8ba0634df4e75358a407e82f419b37440f9c3
glassshoes <nzrmtjhz@outlook.com>
2025-11-13 21:11:19 +0800

    T

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 app = FastAPI()
+DIST_DIR = Path(__file__).resolve().parent / "dist"
 
 @app.get('/')
 async def root():
-    app.mount('/', StaticFiles(directory='dist', html=True),name="static")
+    app.mount('/', StaticFiles(directory=DIST_DIR, html=True),name="static")
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
